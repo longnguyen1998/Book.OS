457a8190a423901d46f6bc441a4bc592cf8441ab
ThanhLong <thanhlong121998@gmail.com>
2019-11-06 17:51:46 +0700

    Initial Commit

---
+++ MyLocations_App/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocations_App
+//
+//  Created by Nguyen Dinh Thanh Long on 11/6/19.
+//  Copyright © 2019 Nguyen Dinh Thanh Long. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
